84dff03d15c9d38e900ee7fcfecf7ab6a7723d37
Raushan Raj <shemnt.raj38@gmail.com>
2016-03-02 18:40:09 +0530

    Update utils.py
    The previous one was not working.Even in verify_hash keys.reverse() throws error because local variable keys was not define.Above one is tested and is working.

--- PayU/payu/utils.py
+++ PayU/payu/utils.py
@@ -1,23 +1,32 @@
+from mezzanine.conf import settings
 from hashlib import sha512
-from django.conf import settings
-KEYS = ('key', 'txnid', 'amount', 'productinfo', 'firstname', 'email',
+KEYS = ['key', 'txnid', 'amount', 'productinfo', 'firstname', 'email',
         'udf1', 'udf2', 'udf3', 'udf4', 'udf5',  'udf6',  'udf7', 'udf8',
-        'udf9',  'udf10')
+        'udf9',  'udf10']
+
+SALT=settings.PAYU_INFO.get('merchant_salt')
 
 def generate_hash(data):
-    keys = ('key', 'txnid', 'amount', 'productinfo', 'firstname', 'email',
-            'udf1', 'udf2', 'udf3', 'udf4', 'udf5',  'udf6',  'udf7', 'udf8',
-            'udf9',  'udf10')
     hash = sha512('')
+    all_str=''
     for key in KEYS:
-        hash.update("%s%s" % (str(data.get(key, '')), '|'))
-    hash.update(settings.PAYU_INFO.get('merchant_salt'))
+        all_str +=str(data.get(key,''))+"|"
+    all_str += SALT
+    hash=sha512(all_str)
+    print hash.hexdigest().lower()
     return hash.hexdigest().lower()
 
-def verify_hash(data, SALT):    
+def verify_hash(data):
+    keys = ['key', 'txnid', 'amount', 'productinfo', 'firstname', 'email',
+        'udf1', 'udf2', 'udf3', 'udf4', 'udf5',  'udf6',  'udf7', 'udf8',
+        'udf9',  'udf10']
     keys.reverse()
-    hash = sha512(settings.PAYU_INFO.get('merchant_salt'))
-    hash.update("%s%s" % ('|', str(data.get('status', ''))))
-    for key in KEYS:
-        hash.update("%s%s" % ('|', str(data.get(key, ''))))
+    all_str=''
+    all_str +=SALT+"|"
+    all_str +=str(data.get('status', ''))
+    for key in keys:
+        all_str+="|"+str(data.get(key,''))
+    hash=sha512(all_str)
     return (hash.hexdigest().lower() == data.get('hash'))
+
+
